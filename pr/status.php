e857790b50d6c2196ab6d5fd3c09da45a2d33d49
harshchauhan11 <harsh.h.chauhan@gmail.com>
2017-03-04 16:15:23 +0530

    status.php

---
+++ pr/status.php
@@ -0,0 +1,25 @@
+<?php
+$dname = $_GET["name"];
+
+$servername = "localhost";
+$username = "adminHuTkSSb";
+$password = "GMgvKYB5qknf";
+$dbname = "preface";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM devices WHERE dname='" . $dname . "'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo $row["dstatus"];
+    }
+} else {
+    echo "0";
+}
+$conn->close();
+?>

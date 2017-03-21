57c07a322dc54cb16080c074d00a164b138d92c3
harshchauhan11 <harsh.h.chauhan@gmail.com>
2017-03-22 03:55:10 +0530

    pr.php

--- pr/updateMin.php
+++ pr/updateMin.php
@@ -1,7 +1,6 @@
 <?php
 require 'db.php';
-//session_start();
-//$mapid = $_GET['pmapid'];
+
 $uid = $_GET['uid'];
 $mapid = '';
 $mname = $_GET['pmname'];
@@ -11,7 +10,7 @@ $dstatus = $_GET['pdstatus'];
 
 $result1 = $mysqli->query("SELECT * FROM mindevices WHERE mapdid=(SELECT mapid FROM minmodules WHERE minumid=$uid AND mname='$mname') AND dname='$dname'") or die($mysqli->error());
 
-// (device + module) successfully found for this user.
+/* (device + module) successfully found for this user. */
 if ( $result1->num_rows > 0 ) {
     while($row1 = $result1->fetch_assoc()) {
         $mapid = row1["mapdid"];
@@ -28,16 +27,6 @@ else {
     echo 2;   
 }
 
-/*
-$result = $mysqli->query("UPDATE mindevices SET dstatus='$dstatus' WHERE mapdid='$mapid' AND dpin='$dpin'");
-
-//echo $mysqli->affected_rows;
-if ($mysqli->affected_rows != 0) {
-    echo 1;
-} else {
-    echo 0;
-}
-*/
-
+echo 3;
 ?>
 

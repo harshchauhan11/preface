95bccfeceb50ba68ec75de1a249a2c45eeb37eaa
harshchauhan11 <harsh.h.chauhan@gmail.com>
2017-05-28 21:59:39 +0530

    Moving Minnie

--- pr/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Database connection settings */
-$host = 'localhost';
-$user = 'admine8NgaEg';
-$pass = 'P5vVcfKb4Ixz';
-$db = 'minthings';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
--- pr/json.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo '[{"mapdid":"esp201318","dname":"led1","dtype":"digital","dpin":"1","dstatus":"off"},{"mapdid":"esp201318","dname":"led2","dtype":"digital","dpin":"2","dstatus":"off"}]';
-
-?>
